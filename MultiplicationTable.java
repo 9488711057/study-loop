180d584cd56627c6e40979bb69985404842c8768
Sri Sankari U <srisankariumapathi@gmail.com>
2026-08-10 10:25:26 +0530

    Add files via upload

---
+++ MultiplicationTable.java
@@ -0,0 +1,14 @@
+package Loop;
+
+import java.util.*;
+public class MultiplicationTable {
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        for(int i=1;i<=m;i++){
+            System.out.println(i+"*"+n+"="+i*n);
+        } 
+        sc.close();
+    }
+}
